55a1a1b316bdaf4152aedd00bbc76a0314019965
Khokon M <khokonads@gmail.com>
2025-06-11 21:59:03 +0600

    Remove sample blog posts; update site metadata with new email, social links and gisqus configuration.

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -12,9 +12,9 @@ const siteMetadata = {
   siteLogo: `${process.env.BASE_PATH || ''}/static/images/logo.png`,
   socialBanner: `${process.env.BASE_PATH || ''}/static/images/mindmire-banner.png`,
   mastodon: '',
-  email: '',
+  email: 'hello@khokon.dev',
   github: 'https://github.com/khokonm',
-  x: 'https://twitter.com/Mindmire',
+  x: 'https://x.com/Md_khokon_M',
   // twitter: 'https://twitter.com/Mindmire',
   facebook: '',
   youtube: '',
@@ -53,7 +53,7 @@ const siteMetadata = {
   newsletter: {
     // supports mailchimp, buttondown, convertkit, klaviyo, revue, emailoctopus, beehive
     // Please add your .env file and modify it according to your selection
-    provider: 'buttondown',
+    provider: '',
   },
   comments: {
     // If you want to use an analytics provider you have to add it to the
@@ -64,10 +64,10 @@ const siteMetadata = {
     giscusConfig: {
       // Visit the link below, and follow the steps in the 'configuration' section
       // https://giscus.app/
-      repo: process.env.NEXT_PUBLIC_GISCUS_REPO,
-      repositoryId: process.env.NEXT_PUBLIC_GISCUS_REPOSITORY_ID,
-      category: process.env.NEXT_PUBLIC_GISCUS_CATEGORY,
-      categoryId: process.env.NEXT_PUBLIC_GISCUS_CATEGORY_ID,
+      repo: 'khokonm/mindmire.com',
+      repositoryId: 'R_kgDOO59EbQ',
+      category: 'General',
+      categoryId: 'DIC_kwDOO59Ebc4CrWJ8',
       mapping: 'pathname', // supported options: pathname, url, title
       reactions: '1', // Emoji reactions: 1 = enable / 0 = disable
       // Send discussion metadata periodically to the parent window: 1 = enable / 0 = disable
